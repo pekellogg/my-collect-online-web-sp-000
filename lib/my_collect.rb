1e2021b6455361b97e7ed3eddf5d631bc1568f24
pekellogg <pekellogg@hotmail.com>
2021-04-21 16:00:22 -0700

    commit tree 2fe0529b52883b4ea66c0a1e66c40bb2a31a2bc4

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+    i = 0
+    collection_mod = []
+    while i < collection.length
+        collection_mod << yield(collection[i])
+        i += 1
+    end
+    collection_mod
+end
 
